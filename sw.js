7b322683f34a68b8965f62f90d184e6591865679
Daniel <martinsdaniel1201@gmail.com>
2026-06-10 21:23:43 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. TODA VEZ que você mudar algo no HTML/CSS, mude essa versão (ex: v2, v3, v4...)
-const CACHE_NAME = 'controlador-despesas-v4'; 
+const CACHE_NAME = 'controlador-despesas-v4.1'; 
 
 const ASSETS = [
   './',
